fce7f8498f420e28c16232f7c72138ae4644b0a7
Luke Warkentin <theloneneutrino@gmail.com>
2024-10-12 13:37:28 -0700

    Made the dice part.  DiceLoop is kindof badly made

--- diceGame.m
+++ diceGame.m
@@ -11,3 +11,23 @@ diceNumChars(diceFilter) = diceInput(diceFilter);
 
 nums = str2num(diceNumChars);
 
+oddidx = @(v) v(1:2:end);
+evnidx = @(v) v(2:2:end);
+
+diceAmounts = oddidx(nums);
+diceConfigs = evnidx(nums);
+
+rands = [];
+diceConfigurations = [];
+for i = 1:size(diceAmounts,2)
+	randum = randi(diceConfigs(i),1,diceAmounts(i));
+	rands = [rands, randum];
+end
+
+index = 1;
+for i = 1:size(diceAmounts,2)
+	for j = index:index + diceAmounts(i) - 1
+		fprintf("%g sided die: %g\n",diceConfigs(i),rands(j));
+	end
+	index = index + diceAmounts(i);
+end
---
+++ diceLoop.m
@@ -0,0 +1,4 @@
+for speed = 1:9223372036854775806
+	clear
+	diceGame
+end
